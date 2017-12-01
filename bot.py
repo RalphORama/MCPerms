88b87a5487d565bfaf530086d834db542560a891
Ralph Drake <rama.o.ralph@gmail.com>
2017-12-01 18:21:53 -0500

    Moved helper functions for the bot into their own library file.

--- bot.py
+++ bot.py
@@ -1,154 +1,51 @@
-"""Summary
+"""MCPerms: A Discord -> Minecraft permission syncing bot.
 
 Attributes:
-    bot (TYPE): Description
-    g_fieldnames (TYPE): Description
-    g_panel (TYPE): Description
-    g_roles (dict): Description
-    g_sid (str): Description
+    bot (discord.ext.commands.bot.Bot): The bot, of course.
+    g_fieldnames (list): List of fieldnames for `data/claimed.csv`.
+    g_panel (pterodactyl.Pterodactyl): API wrapper class for Pterodactyl panel.
+    g_roles (dict): List of Discord role IDs and their associated PEX groups.
+    g_sid (str): Short UUID of the Minecraft server hosted by Pterodactyl.
 """
 import asyncio
-import csv
-import discord
 import json
 import os
 import lib.mcapi as mcapi
+from lib.mcpermshelper import MCPermsHelper
 from lib.pterodactyl import Pterodactyl
-from shutil import copy2
+from os.path import join
 from sys import exit
 from discord.ext import commands
 
 bot = commands.Bot(command_prefix=commands.when_mentioned)
-g_roles = {}
-g_panel = None
-g_sid = ''
+g_helper = None
 g_fieldnames = ['UUID', 'USERNAME', 'DISCORD_ID', 'DISCORD_NICK',
                 'DISCORD_MENTION']
 
 
-def create_data_if_not_exists(folder: str, files: list):
-    """Create the folder structure and files we need for the bot.
-
-    Args:
-        folder (str): The folder to create files within.
-        files (list): The files to create.
-    """
-    if not os.path.exists(folder):
-        os.makedirs(folder)
-
-    for file in files:
-        path = '{}/{}'.format(folder, file)
-        if not os.path.exists(path):
-            with open(path, 'w+') as f:
-                f.write('')
-
-
-def copy_from_example(filename: str):
-    """Create a copy of an example file for the user to edit.
-    I.e. cfg/config-example.json -> cfg/config.json
-
-    Args:
-        filename (str): The file to create from a template.
-    """
-    name = filename[0:filename.index('.')]
-    ext = filename[filename.index('.'):]
-    source = '' + name + '-example' + ext
-    dest = '' + filename
-
-    copy2(source, dest)
-
-
-def instantiate_csv(filepath: str, fieldnames: list):
-    """Instantiate a CSV with the specified field names.
-
-    Args:
-        filepath (str): The full path of the file to populate.
-        fieldnames (list): The fieldnames to set as the header.
-    """
-
-    if not (os.path.isfile(filepath) and
-            os.stat(filepath).st_size > 0):
-        with open(filepath, 'w+', newline='') as file:
-            writer = csv.DictWriter(file, fieldnames=fieldnames)
-            writer.writeheader()
-
-
-def check_claim_eligibility(author: discord.User, server: discord.Server, uuid: str) -> str:
-    """Loop through `data/claimed.csv` to make sure a user isn't claiming a
-    second or duplicate account.
-
-    Args:
-        author (discord.User): The user we're checking.
-        server (discord.Server): The server the claim originated from.
-        uuid (str): The UUID of the Minecraft account.
-
-    Returns:
-        str: Message describing any or lack of conflicts.
-    """
-    with open('data/claimed.csv', 'r', newline='') as claimed_file:
-        reader = csv.DictReader(claimed_file)
-
-        for row in reader:
-            # Respond whether we get a UUID match, Discord ID match,
-            # or no match
-
-            if (row['DISCORD_ID'] == author.id):
-                msg = 'You already registered account {} [`{}`].'
-                return msg.format(row['USERNAME'], row['UUID'])
-            elif (row['UUID'] == uuid):
-                if server.get_member(row['DISCORD_ID']):
-                    tag = row['DISCORD_MENTION']
-                else:
-                    tag = row['DISCORD_NICK']
-
-                msg = '{} already claimed account {} [`{}`].'
-                return msg.format(tag, row['USERNAME'], row['UUID'])
-
-        # The for loop doesn't execute if there are no rows in the file.
-        # Thus, this part is outside to catch both 'not found' and
-        # 'fresh file' cases.
-        msg = 'OK for you to register this nick with this account.'
-        return msg
-
-
-def add_claimed_account(author: discord.User, username: str, uuid: str):
-    """Add a claimed accounts to `data/claimed.csv`
-
-    Args:
-        author (discord.User): The user whose ID and name we're adding.
-        username (str): The username of the Minecraft account.
-        uuid (str): The UUID of the Minecraft account.
-    """
-    with open('data/claimed.csv', 'a', newline='') as claimed_file:
-        writer = csv.DictWriter(claimed_file, fieldnames=g_fieldnames)
-
-        writer.writerow({
-            'UUID': uuid,
-            'USERNAME': username,
-            'DISCORD_ID': author.id,
-            'DISCORD_NICK': author.name + author.discriminator,
-            'DISCORD_MENTION': author.mention
-        })
-
-
 @bot.event
 async def on_ready():
-    """Summary
+    """Code to execute once the bot has successfully logged in to Discord.
     """
+    invite_link = ('https://discordapp.com/oauth2/authorize'
+                   '?client_id={}'
+                   '&scope=bot&permissions=67584').format(bot.user.id)
+
     print('Logged in as {}'.format(bot.user))
-    print('Loaded {} roles.'.format(len(g_roles.keys())))
+    print('Loaded {} roles.'.format(len(g_helper.roles.keys())))
+    print('Invite me to your server:\n    {}\n'.format(invite_link))
 
 
 @bot.command(pass_context=True)
-async def claim(ctx, username):
-    """Summary
+async def claim(ctx, username: str):
+    """Command to claim minecraft permissions.
 
     Args:
-        ctx (TYPE): Description
-        username (TYPE): Description
+        ctx (TYPE): Context of the invoking command.
+        username (str): The Minecraft username to claim.
 
     Returns:
-        TYPE: Description
+        TYPE: Nothing. Not even `None`. Just nothing.
     """
     channel = ctx.message.channel
     a = ctx.message.author
@@ -160,7 +57,7 @@ async def claim(ctx, username):
 
     # Check account UUID and user's Discord ID to make sure neither
     # has (been) registered.
-    eligible = check_claim_eligibility(a, ctx.message.server, uuid)
+    eligible = g_helper.check_claim_eligibility(a, ctx.message.server, uuid)
 
     if 'claimed' in eligible or 'registered' in eligible:
         await bot.send_message(channel, eligible)
@@ -168,22 +65,23 @@ async def claim(ctx, username):
 
     # Loop though each group our user is in and give them the role(s) those
     # groups make them eligble for.
+    roles = g_helper.roles
     roles_added = []
 
     for role in ctx.message.author.roles:
 
-        if role.id in g_roles:
-            for group in g_roles[role.id]:
+        if role.id in roles:
+            for group in roles[role.id]:
                 cmd = 'pex group {} user add {}'.format(group, uuid)
 
                 # TODO: Make this failure case more verbose.
-                if g_panel.send_command(g_sid, cmd):
+                if g_helper.panel.send_command(g_helper.sid, cmd):
                     roles_added.append(group)
 
     if not roles_added:
         await bot.send_message(channel, "You're not eligible for any groups.")
     else:
-        add_claimed_account(a, username, uuid)
+        g_helper.add_claimed_account(a, username, uuid)
 
         msg = 'Added {} to {} role(s): **'.format(username, len(roles_added))
         msg = msg + ', '.join(roles_added)
@@ -192,17 +90,20 @@ async def claim(ctx, username):
 
 
 if __name__ == '__main__':
-    data_files = ['claimed.csv']
-    create_data_if_not_exists('data', data_files)
-    for file in data_files:
-        if file[file.index('.'):] == '.csv':
-            instantiate_csv('data/' + file, g_fieldnames)
+    # # Create all the data files we need.
+    # data_files = ['claimed.csv']
+    # create_data_if_not_exists('data', data_files)
+    # for file in data_files:
+    #     if file[file.index('.'):] == '.csv':
+    #         instantiate_csv('data/' + file, g_fieldnames)
 
+    # Create a template config file and quit with an error if a config file
+    # doesn't exist.
     cfg_files = ['cfg/config.json', 'cfg/roles.json']
     for file in cfg_files:
         # Throw an error and exit if our critical config files don't exist
         if not os.path.isfile(file):
-            copy_from_example(file)
+            MCPermsHelper.copy_from_example(file, join(os.getcwd(), 'cfg'))
             exit('{} not found! I just created it for you. Go fill it out.'
                  .format(file))
 
@@ -210,10 +111,12 @@ if __name__ == '__main__':
         cfg = json.load(cfg_file)
 
     with open('cfg/roles.json', 'r') as roles_file:
-        g_roles = json.load(roles_file)
+        roles = json.load(roles_file)
 
-    g_panel = Pterodactyl(cfg['pubkey'], cfg['privkey'], cfg['baseURL'])
-    g_sid = cfg['serverID']
+    panel = Pterodactyl(cfg['pubkey'], cfg['privkey'], cfg['baseURL'])
+    g_helper = MCPermsHelper(roles, panel, cfg['serverID'], g_fieldnames)
 
-    # https://discordapp.com/oauth2/authorize?client_id=385950397493280805&scope=bot&permissions=67584
+    g_helper.create_data_if_not_exists('data', ['claimed.csv'])
+    g_helper.instantiate_csv('claimed.csv', g_helper.fieldnames)
+
     bot.run(cfg['token'])
---
+++ lib/mcpermshelper.py
@@ -0,0 +1,128 @@
+import csv
+import discord
+import os
+from .pterodactyl import Pterodactyl
+from os.path import abspath, join, dirname, exists, isfile
+from shutil import copy2
+
+
+class MCPermsHelper():
+    def __init__(self, roles: dict, panel: Pterodactyl, sid: str,
+                 fieldnames: list, basedir=''):
+        self.roles = {}
+        self.panel = panel
+        self.sid = sid
+        self.fieldnames = fieldnames
+
+        self.basedir = abspath(join(dirname(__file__), '..'))
+        self.claimfile = join(self.basedir, 'data', 'claimed.csv')
+
+    def create_data_if_not_exists(self, folder: str, files: list):
+        """Create the folder structure and files we need for the bot.
+
+        Args:
+            folder (str): The folder to create files within.
+            files (list): The files to create.
+        """
+        fullpath = join(self.basedir, folder)
+
+        if not exists(fullpath):
+            os.makedirs(fullpath)
+
+        for file in files:
+            path = join(fullpath, file)
+            if not exists(path):
+                with open(path, 'w+') as f:
+                    f.write('')
+
+    def instantiate_csv(self, filename: str, fieldnames: list):
+        """Instantiate a CSV with the specified field names.
+
+        Args:
+            filepath (str): The full path of the file to populate.
+            fieldnames (list): The fieldnames to set as the header.
+        """
+        filepath = join(self.basedir, 'data', filename)
+
+        if not (isfile(filepath) and
+                os.stat(filepath).st_size > 0):
+            with open(filepath, 'w+', newline='') as file:
+                writer = csv.DictWriter(file, fieldnames=fieldnames)
+                writer.writeheader()
+
+    def check_claim_eligibility(self,
+                                author: discord.User,
+                                server: discord.Server,
+                                uuid: str) -> str:
+        """Loop through `data/claimed.csv` to make sure a user isn't claiming a
+        second or duplicate account.
+
+        Args:
+            author (discord.User): The user we're checking.
+            server (discord.Server): The server the claim originated from.
+            uuid (str): The UUID of the Minecraft account.
+
+        Returns:
+            str: Message describing any or lack of conflicts.
+        """
+        with open(self.claimfile, 'r', newline='') as claimed_file:
+            reader = csv.DictReader(claimed_file)
+
+            for row in reader:
+                # Respond whether we get a UUID match, Discord ID match,
+                # or no match
+
+                if (row['DISCORD_ID'] == author.id):
+                    msg = 'You already registered account {} [`{}`].'
+                    return msg.format(row['USERNAME'], row['UUID'])
+                elif (row['UUID'] == uuid):
+                    if server.get_member(row['DISCORD_ID']):
+                        tag = row['DISCORD_MENTION']
+                    else:
+                        tag = row['DISCORD_NICK']
+
+                    msg = '{} already claimed account {} [`{}`].'
+                    return msg.format(tag, row['USERNAME'], row['UUID'])
+
+            # The for loop doesn't execute if there are no rows in the file.
+            # Thus, this part is outside to catch both 'not found' and
+            # 'fresh file' cases.
+            msg = 'OK for you to register this nick with this account.'
+            return msg
+
+    def add_claimed_account(self,
+                            author: discord.User,
+                            username: str,
+                            uuid: str):
+        """Add a claimed accounts to `data/claimed.csv`
+
+        Args:
+            author (discord.User): The user whose ID and name we're adding.
+            username (str): The username of the Minecraft account.
+            uuid (str): The UUID of the Minecraft account.
+        """
+        with open(self.claimfile, 'a', newline='') as claimed_file:
+            writer = csv.DictWriter(claimed_file, fieldnames=g_fieldnames)
+
+            writer.writerow({
+                'UUID': uuid,
+                'USERNAME': username,
+                'DISCORD_ID': author.id,
+                'DISCORD_NICK': author.name + author.discriminator,
+                'DISCORD_MENTION': author.mention
+            })
+
+    @staticmethod
+    def copy_from_example(filename: str, parent_dir: str):
+        """Create a copy of an example file for the user to edit.
+        I.e. cfg/config-example.json -> cfg/config.json
+
+        Args:
+            filename (str): The file to create from a template.
+        """
+        name = filename[0:filename.index('.')]
+        ext = filename[filename.index('.'):]
+        source = join(parent_dir, '' + name + '-example' + ext)
+        dest = join(parent_dir, '' + filename)
+
+        copy2(source, dest)
